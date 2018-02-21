d0e75a885d8f8c3b3d641a8917449421530d2f32
YZT <771030265@qq.com>
2018-02-21 17:03:48 +0800

    配置文件修改

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,6 +11,13 @@ export default {
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+}
+html,body{
+  height: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import axios from 'axios'
 Vue.config.productionTip = false
-
+require('es6-promise').polyfill();
+const axiosInstance = axios.create({
+  baseURL: 'http://lecture.hduhelp.com/',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded/api/v1' }
+})
+Vue.prototype.$ajax = axiosInstance;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
